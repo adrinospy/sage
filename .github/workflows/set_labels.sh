4daff4fd7e83919d515b53fa7738fb51b8cbf38b
Aman Moon <amanmoon099@gmail.com>
2024-05-27 13:46:46 +0530

    Update set_labels.sh

--- .github/workflows/set_labels.sh
+++ .github/workflows/set_labels.sh
@@ -48,6 +48,7 @@ API_URL="https://api.github.com/repos/$REPOSITORY/issues/$PR_NUMBER/labels"
 
 # 'CI Manager' label
 CHANGED_PATH=$(git diff --name-only $PR_BASE_SHA $PR_HEAD_SHA)
+echo $CHANGED_PATH
 array=()
 while IFS= read -r line || [[ -n $line ]]; do
     array+=("$line")
